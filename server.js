74279de20a41c3b892873153105951331ca214f9
tatoGI <workspace.tato@gmail.com>
2025-10-04 02:48:19 +0400

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ const port = process.env.PORT || 3000;
 
 // Enable history API fallback for SPA routing
 app.use(history());
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://www.momtabare.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-XSRF-TOKEN');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  next();
+});
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist'), {
   index: 'index.html'
